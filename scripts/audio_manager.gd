28d31a56a4baca4f53fc78e97d2c69d0c68f2ace
fkeyz <fkeyzuwu@gmail.com>
2026-01-31 14:35:31 +0300

    added file and basic audio manager

---
+++ scripts/audio_manager.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var generic_pickup: AudioStreamPlayer = $GenericPickup
+@onready var forest_ambient: AudioStreamPlayer = $ForestAmbient
+@onready var spider_pickup: AudioStreamPlayer = $SpiderPickup
+@onready var crystal_ball_pickup: AudioStreamPlayer = $CrystalBallPickup
+@onready var stick_pickup: AudioStreamPlayer = $StickPickup
+@onready var water_bucket_pickup: AudioStreamPlayer = $WaterBucketPickup
+@onready var main_music: AudioStreamPlayer = $MainMusic
+@onready var break_player_mask: AudioStreamPlayer = $BreakPlayerMask
+@onready var avatar_colored: AudioStreamPlayer = $AvatarColored
